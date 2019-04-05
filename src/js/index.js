b522a2e964dbcedb94d50731b26462237602a0bf
Maikel Sanchez <maikel.sanchezguti@gmail.com>
2019-04-05 23:23:26 +0000

    my secong repository

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+import $ from "jquery";
+import "bootstrap"; //breathecode dom for more explicit errors
+//import 'breathecode-dom'; //DOM override to make JS easier to use
+import "../style/index.scss";
+
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My granma", "His turtle", "My bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return who[whoIndex] + " " + what[whatIndex] + " " + when[whenIndex];
+};
